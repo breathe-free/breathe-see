90ffba053c608d642b0298e183f897c475475d60
Richard Morrison <richard.morrison@owlstone.co.uk>
2015-03-20 12:53:57 +0000

    The example publisher now pushes realistic breath sample data

--- example_publisher/main.py
+++ example_publisher/main.py
@@ -4,32 +4,59 @@ import sys
 import os
 import time
 import random
+import csv
 
-server_address = '/tmp/lucidity.socket'
+# csv file columns are timestamp, pressure, CO2, ...
+EXAMPLE_DATA = os.path.join(os.path.dirname(__file__), "1426701684-sample-breathing.csv")
+SOCKET_PATH = '/tmp/lucidity.socket'
 
-# Make sure the socket exists
-if not os.path.exists(server_address):
-    raise Exception("No socket at %s" % server_address)
+class SocketNotFound(Exception):
+    pass
 
-# Create a UDS socket
-sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+# Read in data from the example csv file
+datapoints = []
+with open(EXAMPLE_DATA, 'rb') as csvfile:
+    datareader = csv.reader(csvfile)
+    for row in datareader:
+        datapoints.append([float(x) for x in row])
 
-# Try and connect to socket.  Output any error and fall over.
+# Try and connect to socket.  If any error, print out error and output to stdout instead.
 try:
-    sock.connect(server_address)
-except socket.error, msg:
-    print >>sys.stderr, msg
-    sys.exit(1)
+    # Make sure the socket exists
+    if not os.path.exists(SOCKET_PATH):
+        raise SocketNotFound("No socket at %s" % SOCKET_PATH)
 
-# Until user hits Ctrl+C, generate 1000 datapoints every so often
-# and push them down the socket.
+    # Create a UDS socket
+    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+    sock.connect(SOCKET_PATH)
+    output = sock.sendall
+
+except (SocketNotFound, socket.error), msg:
+    print >>sys.stderr, msg
+    print >>sys.stderr, "Will output to STDOUT instead of socket, starting in 2 sec."
+    time.sleep(2)
+
+    def printout(s):
+        print >>sys.stdout, s
+    output = printout
+
+# Until user hits Ctrl+C, send sample data down the socket.
+index = 0
 while True:
     try:
-        data = []
-        for i in range(0,999):   # 1000 data points
-            data.append(str(random.randint(100,900)))
-        
-        sock.sendall(",".join(data) + "\n")
+        datapoint = datapoints[index]
+
+        # Replace the first member of datapoint with the current timestamp
+        datapoint[0] = time.time()
+
+        output( ",".join([str(x) for x in datapoint]) )
+
+        # increment index and loop back round
+        index += 1
+        if index >= len(datapoints):
+            index = 0
+
         time.sleep(0.2)
     except KeyboardInterrupt:
         break
